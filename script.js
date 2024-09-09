9b73a2d67bd023d8db862438660c4989fd245853
alkadabna <abd-ellah.hioun@laplateforme.io>
2024-09-09 11:00:55 +0200

    finished job  3 and learen how i can use only one page of js for 2 php files

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
 window.onload = function(){
+    if (window.location.href.includes("job3")) {
+        console.log("This is job 3: Hello Javascript in the console!");
+    } else if (window.location.href.includes("job2")) {
+        alert("This is job 2: Hello Javascript in an alert!");
+    }
     alert("hello world");
-}
+};
 
