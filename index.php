7e2223673865505d2fd10c6121ad3a1bdcbe5504
alkadabna <abd-ellah.hioun@laplateforme.io>
2024-09-09 10:41:45 +0200

    finished job 1

--- index.php
+++ index.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>job1</title>
-    <script>
-        
+    <script type="text/javascript">
+        window.onload = function(){
+            alert("hello world");
+        }
+
     </script>
 </head>
 <body>
-    
+    <h1>
+        wassup
+    </h1>
 </body>
 </html>
